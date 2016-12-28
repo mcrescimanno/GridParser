60de502bbee3adab5bef160e22c5a7ecd5f11389
Matthew <mcres@Matthews-MacBook-Air-2.local>
2016-12-28 09:46:06 -0500

    gridParser

---
+++ gridParser.py
@@ -0,0 +1,66 @@
+# Matthew Crescimanno (moc22)
+import pdb;
+
+def get_nested_object(obj, key_list):
+    for key in key_list:
+        obj = obj[key]
+    return obj
+
+def process_line(line, pumpkin, key_list):
+    # pdb.set_trace();
+    obj = get_nested_object(pumpkin, key_list) 
+    if '[]' in line or 'intervals:' in line or 'points:' in line:
+        key = line.split(" ")[0].strip('\t:');
+        obj[key] = []
+        key_list.append(key)
+        return key_list
+
+    elif '=' in line:
+        key, value = line.split("=")
+        obj[key.strip()] = value.strip()
+        return key_list
+
+    elif '[' in line and ']' in line:
+        # pdb.set_trace()
+        if not isinstance(obj, list):
+            return process_line(line, pumpkin, key_list[:-1])
+        else:
+            # pdb.set_trace();
+            list_name = line.split(" [")[0].strip()
+            list_number = int(line.split("[")[1].split(']')[0]) - 1
+            parent = get_nested_object(pumpkin, key_list[:-1])
+            if list_name in parent:
+                child_dict = {}
+                obj.append(child_dict) # add dict to list
+                key_list.append(list_number)
+                return key_list
+            else:
+                return process_line(line, pumpkin, key_list[:-1])
+    return key_list
+
+
+def parse(lines):
+    main_obj = {};
+    key_list = [];
+    for line in lines:
+        key_list = process_line(line, main_obj, key_list);
+
+    return main_obj;
+
+def parseFile(filename):
+    with open(filename) as file:
+        return parse(file.readlines())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
